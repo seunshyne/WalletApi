63acf7b38448fd264b0952acb5407fdd7950a010
Oluwaseun Oyebanji <oluwaseunoyebanji4@gmail.com>
2026-02-08 16:03:42 +0100

    CORS Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
 
@@ -20,8 +20,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+
+        $middleware->append(
+            \Illuminate\Http\Middleware\HandleCors::class
+        );
     })
-    
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
